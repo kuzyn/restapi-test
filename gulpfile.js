109374541903494f3b8db142c03e702a7909f712
jsnowball <hello@samuelcousin.com>
2015-05-30 15:46:34 +0200

    tooling + getting data back

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     less = require('gulp-less'),
     path = require('path'),
-    notify = require('gulp-notify');
+    notify = require('gulp-notify')
+    livereload = require('gulp-livereload');
 
+//LESS
 gulp.task('less', function () {
   gulp.src('less/style.less')
     .pipe(less({ paths: [ path.join(__dirname, 'less', 'includes') ]
@@ -15,14 +17,30 @@ gulp.task('less', function () {
     return "Failed to Compile LESS: " + error.message;
   }))
   .pipe(gulp.dest('./'))
-  .pipe(notify("LESS Compiled Successfully :)"));
+  .pipe(livereload());
 });
 
+// HTML 
+gulp.task('html', function() {
+  return gulp.src([ './index.html' ])
+  .pipe(livereload());
+});
+
+// JS 
+gulp.task('js', function() {
+  return gulp.src([ 'js/default.js' ])
+  .pipe(livereload());
+});
+
+//Watch
 gulp.task('watch', function () {
-   gulp.watch('less/**/*', ['less']);
+    livereload.listen()
+    gulp.watch(['js/default.js'], ['js']);
+    gulp.watch(['less/**/*'], ['less']);
+    gulp.watch('./index.html', ['html'])
 });
 
-
+//Build
 gulp.task('build', function () {
   gulp.src('less/style.less')
   .pipe(gulp.dest('./'))
--- js/default.js
+++ js/default.js
@@ -1,6 +1,30 @@
 // Default JavaScript Functions and Initiations
 $(document).ready(function() {
 
-  // Functions go here...
-  
+  // Retrieve clinic
+  var request = new XMLHttpRequest();
+  var endpoint = 'http://private-6e916-medigochallenges.apiary-mock.com/'
+// Event monitoring
+request.addEventListener("load", reqComplete, false);
+request.addEventListener("error", reqError, false);
+function reqError(evt) {
+	console.log("An error occurred.")
+}
+function reqComplete(evt) {
+	console.log("The request was completed.")
+}
+
+request.open('GET', endpoint+'clinics');
+
+  //Ajax callback
+request.onreadystatechange = function() {
+	if (this.readyState === 4) {
+		// console.log('Status:', this.status);
+		// console.log('Headers:', this.getAllResponseHeaders());
+		// console.log('Body:', this.responseText);
+		json = JSON.parse(this.responseText)
+    console.log(json)
+	}
+  };
+  request.send();
 }); // end document ready
