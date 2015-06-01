b3d09ceddc30e69d8ffa0a705b452b5ea0d98363
jsnowball <hello@samuelcousin.com>
2015-06-01 17:44:12 +0200

    last stretch :star:

--- js/default.js
+++ js/default.js
@@ -1,30 +1,36 @@
 // Default JavaScript Functions and Initiations
+//
+//
 $(document).ready(function() {
+
+//### Variables ###
 var apiEp = 'http://private-6e916-medigochallenges.apiary-mock.com/'
 var queryPath = apiEp+'clinics'
+
+// Isotope instance
 var $isoContainer = $('#isoContainer').isotope({
   getSortData: {
-    name: '.name'
+    name: '.name',
+    score: '.score parseInt'
+  },
+  sortAscending: {
+    name: true,
+    score: false
   }
 });
 
-// Populate Array(object.property)
-function populateArray(_o) {
-  var array = []
-  for (var i = 0; i < _o.length; i++) {
-  cleanProcedures = _o[i].name.split(' ').join('_').toLowerCase() //rom
-  array.push(cleanProcedures)
-  }
-  return array
-}
 
-// Helpers
-function populateClinicDiv(_object) {
+
+//### Helpers ###
+
+// Parse object and construct html: buildItemHtml(jsonobject)
+function buildItemHtml(_object) {
+  $('.loader').fadeOut("slow", function() {
   //object properties
   clinicName = _object.name
   clinicId = _object.id
   clinicUrl = _object.url
-  clinicDesc = _object.desc
+  clinicDesc = _object.desc.split(".")[0]+"..." // Only getting the first sentence of desc with the idea of js slidedown of the full desc on click...
   clinicImg = _object.img
   clinicScore = _object.score
   clinicAccreditation = _object.accreditations
@@ -32,70 +38,100 @@ function populateClinicDiv(_object) {
 
   //html
   itemId = 'clinic-'+clinicId
-  itemDiv  = '<div id='+itemId+' class="grid-item col-12 col-tablet-6 col-desktop-4 col-hd-3 '+clinicProcedures+'"></div>'
+  itemDiv  = '<div id='+itemId+' class="grid-item col-6 col-tablet-6 col-desktop-4 col-hd-3 '+clinicProcedures+'"></div>'
   $isoContainer.append(itemDiv)
-  // $isoContainer.append('<div id="clinic-'+clinicId+'" class="grid-item col-12 col-tablet-6 col-desktop-4 col-hd-3 '+clinicProcedures+'">'+_object.name+'</div><div class="cf"></div>')
-  // var $divId = $('#clinic-'+clinicId)
-  // var content =
-  // '<img class="col-12 thumbs" src="'+_object.img+'">'+
-  // '<h4 class="name">'+
-  // _object.name+
-  // '</h4>'+
-  // '<h5>'+
-  // 'Score'+_object.score+
-  // '</h5>'+
-  // '<p>'+
-  // _object.desc+
-  // '</p>'
-  $("#"+itemId).html("<p>dem buuuuugs</p>")
-  $isoContainer.isotope('appended', itemId)
-  $isoContainer.isotope('layout')
-  $isoContainer.isotope({ sortBy : 'name' });
+  
+
+  var htmlContent =
+  '<h3 class="name text-center">'+clinicName+'</h3> \
+  <div class="thumbs no-padding no-margin"><img class="col-12 no-padding no-margin" src="'+clinicImg+'"></div> \
+  <h5 class="col-6">Score: </h5><h5 class="score col-6">'+clinicScore+'</h5> \
+  <p class="hide-mobile show-tablet">'+clinicDesc+'</p>'
+
+  $("#"+itemId).html(htmlContent)
+  $isoContainer.isotope('appended', $("#"+itemId)) //Add item to isotope grid
+  timeoutImageLoad(800) // Kludge fix because isotope doesn't play well with unloaded images :(
+});
 }
 
-// Main fetching function
+// timeoutImageLoad(msdelay) Give time for images to load before reinitializing the isotope layout
+function timeoutImageLoad(_delay) {
+  timeoutID = window.setTimeout(function() {
+  $isoContainer.isotope('layout')  //Reinstanciate isotope layout
+  $isoContainer.isotope({ sortBy : 'name' }) //Order by name by default
+  $isoContainer.fadeTo(500, 1)
+  fadeInElement("#footer", 200)
+  fadeInElement(".thumbs", 400)
+  }, _delay);
+}
+
+// fadeInimages(elementclassname, msdelay)... ideally should be using imageLoaded but couldn't make it work with async
+function fadeInElement(_class, _delay) {
+  $(_class).each(function() {
+    $(this).fadeTo(_delay, 1)
+  })
+}
+
+// populateArray(object.property)
+function populateArray(_object) {
+  var array = []
+  for (var i = 0; i < _object.length; i++) {
+  cleanProcedures = _object[i].name.split(' ').join('_').toLowerCase() //rom
+  array.push(cleanProcedures)
+}
+return array
+}
+
+// Fetch json:  sendRequest(apipath, callback)
 function sendRequest(_path, _cb) {
   req = new XMLHttpRequest()
   req.onerror = reqError
-  req.onload = reqSuccess
   req.callback = _cb
   req.open('GET', _path, true) // true == asyn == obv. makes the request inordered
   req.onreadystatechange = function() {
-      if (this.readyState === 4) {
-        if(this.status === 200) {
-          this.callback(JSON.parse(this.responseText))
-          }
+    if (this.readyState === 4) {
+      if(this.status === 200) {
+        this.callback(JSON.parse(this.responseText))
       }
+    }
   }
   req.send(null)
 }
 
-//Handlers
-function reqSuccess () {
-}
 
+
+//### Handlers ###
+
+// Error handling
 function reqError() {
   console.error(this.statusText);
 }
 
-// filter items on button click
+// Filter items
 $('.filter-button-group a').click( function() {
-  var filterValue = $(this).attr('data-filter')
+  filterValue = $(this).attr('data-filter')
+  event.preventDefault();
   $isoContainer.isotope({ filter: filterValue })
 });
+
+// Sort items
+$('.sort-button-group a').click( function() {
+  sortValue = $(this).attr('data-filter')
+  event.preventDefault();
+  $isoContainer.isotope({ sortBy: sortValue })
+});
+
+
+
+// ### Main ###
 
 // Where the magic happens...
 sendRequest(queryPath, function (_response) { // Callback on clinic list to iterate through them
   for (var i = 0; i <= _response.length - 1; i++) {
-    sendRequest(queryPath+'/'+_response[i], populateClinicDiv) // On 200 of each clinic, call the populateClinicDiv
+    sendRequest(queryPath+'/'+_response[i], buildItemHtml) // On 200 of each clinic, call the buildItemHtml
   }
 })
 
-// $("img").unveil(200, function() {
-//   $(this).load(function() {
-//     this.style.opacity = 1;
-//   });
-// });
 
 
 }); // end document ready
